342db9017f3a4a09720f1d28ae0c93ce9812a20b
Astemir Eleev <astemireleev@gmail.com>
2018-04-04 17:50:58 +0300

    Refactored project structure

--- rope-node-spritekit-ios/Rope Node/RopeNode.swift
+++ rope-node-spritekit-ios/Rope Node/RopeNode.swift
@@ -6,24 +6,7 @@
 //  Copyright © 2018 Astemir Eleev. All rights reserved.
 //
 
-import Foundation
 import SpriteKit
-
-class SpriteRopeNode: SKSpriteNode {
-    
-    // MARK: - Initializers
-    
-    init(nodeTexture: SKTexture) {
-        super.init(texture: nodeTexture, color: UIColor(), size: nodeTexture.size())
-        
-        self.anchorPoint = CGPoint(x: 0.5, y: 1.0)
-        self.physicsBody = SKPhysicsBody(rectangleOf: size)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
 
 class RopeNode: SKNode {
     
