34462e08232651fc376a124af8f6d58576c62e19
Astemir Eleev <astemireleev@gmail.com>
2018-07-10 17:01:59 +0300

    Created UIImage+ImageScale extension

---
+++ destructible-sprite/UIImage+ImageScale.swift
@@ -0,0 +1,40 @@
+//
+//  UIImage+ImageScale.swift
+//  destructible-sprite
+//
+//  Created by Astemir Eleev on 10/07/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    
+    /// Scales an image to fit within a bounds with a size governed by the passed size. Also keeps the aspect ratio.
+    /// Switch MIN to MAX for aspect fill instead of fit.
+    ///
+    /// - parameter newSize: newSize the size of the bounds the image must fit within.
+    ///
+    /// - returns: a new scaled image.
+    func scaleImageToSize(newSize: CGSize) -> UIImage {
+        var scaledImageRect = CGRect.zero
+        
+        let aspectWidth = newSize.width/size.width
+        let aspectheight = newSize.height/size.height
+        
+        let aspectRatio = max(aspectWidth, aspectheight)
+        
+        scaledImageRect.size.width = size.width * aspectRatio;
+        scaledImageRect.size.height = size.height * aspectRatio;
+        scaledImageRect.origin.x = (newSize.width - scaledImageRect.size.width) / 2.0;
+        scaledImageRect.origin.y = (newSize.height - scaledImageRect.size.height) / 2.0;
+        
+        UIGraphicsBeginImageContext(newSize)
+        draw(in: scaledImageRect)
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage!
+    }
+}
