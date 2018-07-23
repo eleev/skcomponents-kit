3d1d2a2f71eed544d611f709766bc2efbc702a35
Astemir Eleev <astemireleev@gmail.com>
2018-07-23 16:37:41 +0300

    Added Controllable2D file

---
+++ joystick-node/Joystick/Controllable2D.swift
@@ -0,0 +1,12 @@
+//
+//  Controllable2D.swift
+//  ios-spritekit-tanks
+//
+//  Created by Astemir Eleev on 10/06/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+/// Protocol composition typealias for convenience
+typealias Controllable2D = MoveControllable & RotateControllable
