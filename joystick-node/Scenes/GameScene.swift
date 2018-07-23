51f65bade6863b78d5fed6fcde21f2e89e191885
Astemir Eleev <astemireleev@gmail.com>
2018-07-23 17:25:53 +0300

    Implemented GameScene with the guidance

--- joystick-node/Scenes/GameScene.swift
+++ joystick-node/Scenes/GameScene.swift
@@ -11,79 +11,70 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // MARK: - Properties
+    
+    private var tankNode : SKSpriteNode?
+    
+    lazy var joystickMovement: Joystick = {
+        let joystick = Joystick()
+        joystick.zPosition = 100
+        joystick.xScale = 2.0
+        joystick.yScale = 2.0
+        return joystick
+    }()
+    
+    lazy var joystickRotation: Joystick = {
+        let joystick = Joystick()
+        joystick.zPosition = 100
+        joystick.xScale = 1.8
+        joystick.yScale = 1.8
+        return joystick
+    }()
+    
+    // MARK: - Overrides
     
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        tankNode = self.childNode(withName: "//Tank") as? SKSpriteNode
+        
+        guard let tankNode = self.tankNode else {
+            return
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // You need to attack the target node to the joystick in order to be able to use it
+        joystickMovement.attach(moveControllable: tankNode)
+        // If you need to control only the position or just orientation - use the corresponding attach method
+        joystickRotation.attach(rotateControllable: tankNode)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        // The next step is to properly position the joystick
+        let controlYPosition = -(scene?.view?.bounds.height)! / 2.5
+        joystickMovement.position = CGPoint(x: -(scene?.view?.bounds.width)! / 2.5, y: controlYPosition)
+        // And finally add it as a child node of the Scene's root node
+        addChild(joystickMovement)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        joystickRotation.position = CGPoint(x: (scene?.view?.bounds.width)! / 2.5, y: controlYPosition)
+        addChild(joystickRotation)
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // Finally the joystick needs to be updated in order to properly function
+        joystickMovement.update(currentTime)
+        joystickRotation.update(currentTime)
+
+    }
+}
+
+// Adds conformance to Controllable2D protocol which already has a number of protocol extensions, so it is not mandatory to implement the conformance
+extension SKSpriteNode: Controllable2D {
+    
+    func rotate(for angularVelocity: CGFloat) {
+        // Delegate the operation to the protocol extnesion method that provides the default implementation for rotation
+        rotate(node: self, using: angularVelocity, with: 0.3, timingMode: .linear, shouldInverse: false)
+    }
+    
+    func move(for velocity: CGPoint) {
+        // Delegate the operation to the protocol extnesion method that provides the default implementation for movement
+        move(node: self, using: velocity, with: 0.3, timingMode: .linear)
     }
 }
